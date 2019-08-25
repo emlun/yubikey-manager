b7eaead021200b0c3ddd93629a86304174249ff6
Emil Lundberg <emil@emlun.se>
2019-08-25 03:43:06 +0200

    Extract module modhex.rs

--- ykman-rs/src/lib.rs
+++ ykman-rs/src/lib.rs
@@ -1,3 +1,7 @@
+mod modhex;
+
+use regex::bytes::Regex;
+
 use pyo3::exceptions::TypeError;
 use pyo3::exceptions::ValueError;
 use pyo3::prelude::pymodule;
@@ -8,49 +12,10 @@ use pyo3::types::PyAny;
 use pyo3::types::PyBytes;
 use pyo3::types::PyString;
 use pyo3::types::PyTuple;
-use pyo3::IntoPyObject;
-use pyo3::PyObject;
 
-use regex::bytes::Regex;
+use modhex::Modhex;
 
 const PEM_IDENTIFIER: &str = &"-----BEGIN";
-
-struct Modhex(String);
-impl Modhex {
-    fn from_bytes(value: &[u8]) -> Modhex {
-        Modhex(value.iter().map(byte_to_modhex_digits).fold(
-            String::new(),
-            |mut result, (msd, lsd)| {
-                result.push(msd);
-                result.push(lsd);
-                result
-            },
-        ))
-    }
-
-    fn from_modhex(value: &str) -> Result<Modhex, ()> {
-        for c in value.chars() {
-            if "cbdefghijklnrtuv".contains(c) == false {
-                return Err(());
-            }
-        }
-        Ok(Modhex(value.to_string()))
-    }
-
-    fn to_bytes(&self) -> Vec<u8> {
-        let msds = self.0.chars().step_by(2);
-        let lsds = self.0.chars().skip(1).step_by(2);
-        msds.zip(lsds)
-            .map(|(msd, lsd)| modhex_digit_to_byte(msd) * 16 + modhex_digit_to_byte(lsd))
-            .collect()
-    }
-}
-
-impl IntoPyObject for Modhex {
-    fn into_object(self, py: Python) -> PyObject {
-        PyString::new(py, &self.0).into_object(py)
-    }
-}
 
 fn pem_regex() -> Regex {
     Regex::new(PEM_IDENTIFIER).expect("Failed to compile regex for PEM signature")
@@ -62,56 +27,6 @@ fn is_cve201715361_vulnerable_firmware_version(ver: (i64, i64, i64)) -> bool {
 
 fn is_pem_bytes(data: &[u8]) -> bool {
     pem_regex().is_match(data)
-}
-
-fn modhex_digit_to_byte(digit: char) -> u8 {
-    match digit {
-        'c' => 0,
-        'b' => 1,
-        'd' => 2,
-        'e' => 3,
-        'f' => 4,
-        'g' => 5,
-        'h' => 6,
-        'i' => 7,
-        'j' => 8,
-        'k' => 9,
-        'l' => 10,
-        'n' => 11,
-        'r' => 12,
-        't' => 13,
-        'u' => 14,
-        'v' => 15,
-        _ => panic!(format!("Invalid modhex digit: {}", digit)),
-    }
-}
-
-fn nibble_to_modhex_digit(n: u8) -> char {
-    match n {
-        0 => 'c',
-        1 => 'b',
-        2 => 'd',
-        3 => 'e',
-        4 => 'f',
-        5 => 'g',
-        6 => 'h',
-        7 => 'i',
-        8 => 'j',
-        9 => 'k',
-        10 => 'l',
-        11 => 'n',
-        12 => 'r',
-        13 => 't',
-        14 => 'u',
-        15 => 'v',
-        _ => panic!(format!("Out of modhex digit range: {}", n)),
-    }
-}
-
-fn byte_to_modhex_digits(i: &u8) -> (char, char) {
-    let msb = i / 16;
-    let lsb = i % 16;
-    (nibble_to_modhex_digit(msb), nibble_to_modhex_digit(lsb))
 }
 
 #[pymodule]
@@ -151,7 +66,7 @@ fn ykman_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "modhex_decode")]
     fn py_modhex_decode<'p>(py: Python<'p>, data: &PyString) -> PyResult<&'p PyBytes> {
         Modhex::from_modhex(data.to_string()?.as_ref())
-            .map(|mh| PyBytes::new(py, mh.to_bytes().as_slice()))
+            .map(|mh| PyBytes::new(py, mh.as_bytes().as_slice()))
             .or_else(|_| Err(ValueError::py_err(format!("Invalid modhex: {:?}", data))))
     }
 
---
+++ ykman-rs/src/modhex.rs
@@ -0,0 +1,99 @@
+use pyo3::prelude::Python;
+use pyo3::types::PyString;
+use pyo3::IntoPyObject;
+use pyo3::PyObject;
+
+pub struct Modhex(String);
+impl Modhex {
+    pub fn from_bytes(value: &[u8]) -> Modhex {
+        Modhex(value.iter().map(byte_to_modhex_digits).fold(
+            String::new(),
+            |mut result, (msd, lsd)| {
+                result.push(msd);
+                result.push(lsd);
+                result
+            },
+        ))
+    }
+
+    pub fn from_modhex(value: &str) -> Result<Modhex, ()> {
+        for c in value.chars() {
+            if "cbdefghijklnrtuv".contains(c) == false {
+                return Err(());
+            }
+        }
+        Ok(Modhex(value.to_string()))
+    }
+
+    pub fn as_bytes(&self) -> Vec<u8> {
+        let msds = self.0.chars().step_by(2);
+        let lsds = self.0.chars().skip(1).step_by(2);
+        msds.zip(lsds)
+            .map(|(msd, lsd)| modhex_digit_to_byte(msd) * 16 + modhex_digit_to_byte(lsd))
+            .collect()
+    }
+}
+
+impl IntoPyObject for Modhex {
+    fn into_object(self, py: Python) -> PyObject {
+        PyString::new(py, &self.0).into_object(py)
+    }
+}
+
+fn modhex_digit_to_byte(digit: char) -> u8 {
+    match digit {
+        'c' => 0,
+        'b' => 1,
+        'd' => 2,
+        'e' => 3,
+        'f' => 4,
+        'g' => 5,
+        'h' => 6,
+        'i' => 7,
+        'j' => 8,
+        'k' => 9,
+        'l' => 10,
+        'n' => 11,
+        'r' => 12,
+        't' => 13,
+        'u' => 14,
+        'v' => 15,
+        _ => panic!(format!("Invalid modhex digit: {}", digit)),
+    }
+}
+
+fn nibble_to_modhex_digit(n: u8) -> char {
+    match n {
+        0 => 'c',
+        1 => 'b',
+        2 => 'd',
+        3 => 'e',
+        4 => 'f',
+        5 => 'g',
+        6 => 'h',
+        7 => 'i',
+        8 => 'j',
+        9 => 'k',
+        10 => 'l',
+        11 => 'n',
+        12 => 'r',
+        13 => 't',
+        14 => 'u',
+        15 => 'v',
+        _ => panic!(format!("Out of modhex digit range: {}", n)),
+    }
+}
+
+fn byte_to_modhex_digits(i: &u8) -> (char, char) {
+    let msb = i / 16;
+    let lsb = i % 16;
+    (nibble_to_modhex_digit(msb), nibble_to_modhex_digit(lsb))
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
