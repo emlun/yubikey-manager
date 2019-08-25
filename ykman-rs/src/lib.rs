b7c4cd03f4b0b9924c6fd77f9e5cd295f2ce1291
Emil Lundberg <emil@emlun.se>
2019-08-25 02:01:18 +0200

    Extract pure-rust function is_pem_bytes

--- ykman-rs/src/lib.rs
+++ ykman-rs/src/lib.rs
@@ -1,4 +1,3 @@
-use pyo3::prelude::pyfunction;
 use pyo3::prelude::pymodule;
 use pyo3::prelude::PyModule;
 use pyo3::prelude::PyResult;
@@ -6,7 +5,6 @@ use pyo3::prelude::Python;
 use pyo3::types::PyAny;
 use pyo3::types::PyBytes;
 use pyo3::types::PyString;
-use pyo3::wrap_pyfunction;
 
 use regex::bytes::Regex;
 
@@ -16,20 +14,22 @@ fn pem_regex() -> Regex {
     Regex::new(PEM_IDENTIFIER).expect("Failed to compile regex for PEM signature")
 }
 
-#[pyfunction]
-fn is_pem(data: &PyAny) -> PyResult<bool> {
-    if let Ok(data) = data.downcast_ref::<PyString>() {
-        Ok(pem_regex().is_match(data.as_bytes()))
-    } else if let Ok(data) = data.downcast_ref::<PyBytes>() {
-        Ok(pem_regex().is_match(data.as_bytes()))
-    } else {
-        Ok(false)
-    }
+fn is_pem_bytes(data: &[u8]) -> bool {
+    pem_regex().is_match(data)
 }
 
 #[pymodule]
 fn ykman_rs(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(is_pem))?;
+    #[pyfn(m, "is_pem")]
+    fn py_is_pem(_py: Python, data: &PyAny) -> PyResult<bool> {
+        if let Ok(data) = data.downcast_ref::<PyString>() {
+            Ok(is_pem_bytes(data.as_bytes()))
+        } else if let Ok(data) = data.downcast_ref::<PyBytes>() {
+            Ok(is_pem_bytes(data.as_bytes()))
+        } else {
+            Ok(false)
+        }
+    }
 
     Ok(())
 }
