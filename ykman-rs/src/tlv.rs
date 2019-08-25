f20d215f828b8a94348e6c6022e90b1e9851c64c
Emil Lundberg <emil@emlun.se>
2019-08-25 16:51:30 +0200

    Extract function add_u128_leading_zeroes

--- ykman-rs/src/tlv.rs
+++ ykman-rs/src/tlv.rs
@@ -1,5 +1,15 @@
 use std::convert::TryInto;
 
+fn add_u128_leading_zeroes(bytes: &[u8], len: usize) -> [u8; 16] {
+    let mut padded: [u8; 16] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for i in 0..len {
+        let result_index: usize = (16 - len + i).try_into().unwrap();
+        let input_index: usize = i.try_into().unwrap();
+        padded[result_index] = bytes[input_index];
+    }
+    padded
+}
+
 pub fn parse_tag(data: &[u8], offset: usize) -> (u16, u8) {
     let t: u16 = data[offset].into();
 
@@ -18,13 +28,8 @@ pub fn parse_length(data: &[u8], offset: usize) -> (u128, u8) {
 
     if ln > 0x80 {
         let n_bytes: u8 = ln - 0x80;
-        let mut be_bytes: [u8; 16] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        for i in 0..n_bytes {
-            let be_index: usize = (16 - n_bytes + i).try_into().unwrap();
-            let i_usize: usize = i.try_into().unwrap();
-            let data_index: usize = offset + i_usize;
-            be_bytes[be_index] = data[data_index];
-        }
+        let be_bytes: [u8; 16] =
+            add_u128_leading_zeroes(&data[offset..], n_bytes.try_into().unwrap());
         let ln: u128 = u128::from_be_bytes(be_bytes);
         (ln, n_bytes + 1)
     } else {
